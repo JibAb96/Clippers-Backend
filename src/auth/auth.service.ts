4547993d5fe296a741678ec4dd427054aa5a58ea
JibAb96 <j.abshir96@gmail.com>
2025-04-24 12:52:48 +0200

    Add AuthService for user registration, login, and user deletion functionality

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,50 @@
+import {
+  Injectable,
+  Logger,
+} from '@nestjs/common';
+import { AuthRepository } from './auth.repository';
+import { AuthDto } from './dtos/auth.dto';
+import { AuthResponse } from './interfaces/auth-response.interface';
+@Injectable()
+export class AuthService {
+  private readonly logger = new Logger(AuthService.name);
+  constructor(private readonly authRepository: AuthRepository) {}
+
+  async register(authDto: AuthDto): Promise<AuthResponse> {
+    try {
+      const authData = await this.authRepository.register(
+        authDto.email,
+        authDto.password,
+      );
+      return {
+        id: authData.user?.id,
+        token: authData.session?.access_token,
+        refreshToken: authData.session?.refresh_token,
+      };
+    } catch (error) {
+      this.logger.error(`Registration failed: ${error.message}`, error.stack);
+      throw error;
+    }
+  }
+
+  async signin(authDto: AuthDto): Promise<AuthResponse> {
+    try {
+      const authData = await this.authRepository.login(
+        authDto.email,
+        authDto.password,
+      );
+
+      return {
+        id: authData.user.id,
+        token: authData.session.access_token,
+        refreshToken: authData.session.refresh_token,
+      };
+    } catch (error) {
+      this.logger.error(`Login failed: ${error.message}`, error.stack);
+      throw error;
+    }
+  }
+  async deleteUser(id: string): Promise<void> {
+    await this.authRepository.deleteUser(id);
+  }
+}
