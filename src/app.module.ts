6b08aeba6f57bc80fff7b6721b8f073ca7007e49
JibAb96 <j.abshir96@gmail.com>
2025-04-24 12:49:24 +0200

    Refactor AppModule to include global configuration and validation pipe setup. Add all module dependencies of the app.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,34 @@
-import { Module } from '@nestjs/common';
+import { Module, ValidationPipe } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { ClipperProfilesModule } from './clipper-profiles/clipper-profiles.module';
+import { ClipperProfilesModule } from './clippers/clippers.module';
+import { ConfigModule } from '@nestjs/config';
+import { SupabaseModule } from './supabase/supabase.module';
+import { AuthModule } from './auth/auth.module';
+import { CreatorsModule } from "./creators/creators.module";
+import { APP_PIPE } from "@nestjs/core";
 
 @Module({
-  imports: [UsersModule, ClipperProfilesModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `./src/.env.${process.env.NODE_ENV}`,
+    }),
+    CreatorsModule,
+    ClipperProfilesModule,
+    SupabaseModule,
+    AuthModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_PIPE,
+      useValue: new ValidationPipe({
+        whitelist: true,
+      }),
+    },
+  ],
 })
-export class AppModule {}
+export class AppModule {
+}
