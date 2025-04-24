0b0622731b6f90a1d565ea8bcbd032543f9fbec5
JibAb96 <j.abshir96@gmail.com>
2025-04-24 12:38:07 +0200

    Add Niche enum for categorizing content types.

---
+++ src/enums/niche.enum.ts
@@ -0,0 +1,12 @@
+export enum Niche {
+    GAMING = 'Gaming',
+    SPORT = 'Sport',
+    FASHION = 'Fashion',
+    BEAUTY = 'Beauty',
+    TECHNOLOGY = 'Technology',
+    FITNESS = 'Fitness',
+    TRAVEL = 'Travel',
+    ENTERTAINMENT = 'Entertainment',
+    FOOD = 'Food',
+    OTHER = 'Other'
+}
