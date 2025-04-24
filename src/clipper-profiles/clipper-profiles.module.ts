8da4d7171371c4c30ea1a1f78add73d856bfdaf5
JibAb96 <j.abshir96@gmail.com>
2025-04-24 12:30:31 +0200

    Change server architecture to the match to the  diagram made on draw.io.

--- src/clipper-profiles/clipper-profiles.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ClipperProfilesService } from './clipper-profiles.service';
-import { ClipperProfilesController } from './clipper-profiles.controller';
-
-@Module({
-  providers: [ClipperProfilesService],
-  controllers: [ClipperProfilesController]
-})
-export class ClipperProfilesModule {}
