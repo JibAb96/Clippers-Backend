20897300af77180f774b572e47557b13510c953d
JibAb96 <j.abshir96@gmail.com>
2025-05-01 17:16:19 +0200

    Remove UserResponse interface from auth-response.dto.ts as it is no longer needed for user authentication responses

--- src/interfaces/auth-response.dto.ts
+++
@@ -1,7 +0,0 @@
-import { CreatorProfileInterface } from './creator-profle.interface';
-
-export interface UserResponse {
-  user: CreatorProfileInterface;
-  token?: string;
-  refreshToken?: string;
-}
