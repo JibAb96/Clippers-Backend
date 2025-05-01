10cf4bfd9105e9f9336318f8beed168873bdb242
JibAb96 <j.abshir96@gmail.com>
2025-05-01 17:51:28 +0200

    Remove unused authentication and DTO files to streamline the codebase, enhancing maintainability and reducing clutter.

--- src/auth/decorators/current-user.decorator.ts
+++
@@ -1,9 +0,0 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-import { RequestWithUser } from "../interfaces/auth-request.interface"; 
-
-export const CurrentUser = createParamDecorator(
-  (data: unknown, ctx: ExecutionContext) => {
-    const request = ctx.switchToHttp().getRequest<RequestWithUser>();
-    return request.user;
-  },
-);
--- src/auth/interfaces/upload-response.interface.ts
+++
@@ -1,4 +0,0 @@
-export interface UploadFileResponse{
-    url: string,
-    path: string
-}
