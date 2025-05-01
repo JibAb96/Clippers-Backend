d4c623038149adb0228a89f73097be326d6ecfcc
JibAb96 <j.abshir96@gmail.com>
2025-05-01 17:48:35 +0200

    Add ClippersModule to AuthModule imports for enhanced functionality

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,11 +5,13 @@ import { AuthController } from './auth.controller';
 import { CreatorsModule } from "../creators/creators.module";
 import { UserFacadeService } from "./user-facade.service";
 import { SupabaseModule } from "../supabase/supabase.module";
+import { ClippersModule } from "src/clippers/clippers.module";
 
 @Module({
   providers: [AuthService, UserFacadeService, AuthRepository],
   exports: [AuthService],
   controllers: [AuthController],
-  imports:[CreatorsModule, SupabaseModule]
+  imports: [CreatorsModule, SupabaseModule, ClippersModule
+  ],
 })
 export class AuthModule {}
