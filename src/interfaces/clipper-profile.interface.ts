16ec8f8df2b6841fa410c9d9cfcb87a36b1a47ad
JibAb96 <j.abshir96@gmail.com>
2025-05-01 17:12:08 +0200

    Add ClipperInterface to define structure for clipper profiles, including properties like id, fullName, brandName, email, and social media details

---
+++ src/interfaces/clipper-profile.interface.ts
@@ -0,0 +1,16 @@
+import { Niche } from "../enums/niche.enum";
+import { Platform } from "../enums/platform.enum";
+
+export interface ClipperInterface {
+  id: string;
+  fullName: string;
+  brandName: string;
+  email: string;
+  socialMediaHandle: string;
+  platform: Platform;
+  niche: Niche;
+  country: string;
+  followerCount: number;
+  pricePerPost: number;
+  brandProfilePic: string | null;
+}
