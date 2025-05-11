b18141983e7f7856f305f1ed1ca96c626ffacdf5
JibAb96 <j.abshir96@gmail.com>
2025-05-11 17:11:53 +0200

    Update UserResponse interface to allow null user values and add role property for better user role management.

--- src/interfaces/user-auth-response.interface.ts
+++ src/interfaces/user-auth-response.interface.ts
@@ -2,7 +2,8 @@ import { ClipperInterface } from "./clipper-profile.interface";
 import { CreatorProfileInterface } from './creator-profle.interface';
 
 export interface UserResponse {
-  user: CreatorProfileInterface | ClipperInterface;
+  user: CreatorProfileInterface | ClipperInterface | null;
+  role: "creator" | "clipper";
   token?: string;
   refreshToken?: string;
 }
