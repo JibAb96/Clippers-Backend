551e153a784eebd14033c311fe1bb1eec68edf7e
JibAb96 <j.abshir96@gmail.com>
2025-05-11 17:12:09 +0200

    Update SupabaseModule to include SupabaseAuthClientService in providers and exports, enhancing authentication management capabilities.

--- src/supabase/supabase.module.ts
+++ src/supabase/supabase.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { SupabaseService } from './supabase.service';
-import { ConfigModule } from "@nestjs/config";
+import { ConfigModule } from '@nestjs/config';
+import { SupabaseAuthClientService } from './supabase-auth-client.service';
 
 @Module({
-  imports:[ConfigModule],
-  providers: [SupabaseService],
-  exports:[SupabaseService]
+  imports: [ConfigModule],
+  providers: [SupabaseService, SupabaseAuthClientService],
+  exports: [SupabaseService, SupabaseAuthClientService],
 })
 export class SupabaseModule {}
