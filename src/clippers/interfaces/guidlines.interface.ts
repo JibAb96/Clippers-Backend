2daef8ab6d37d043153da978c81b2a0ae1c53432
JibAb96 <j.abshir96@gmail.com>
2025-05-11 17:06:33 +0200

    Refactor GuidelinesInterface to correct spelling and add GuidelinesResponse interface for enhanced data structure. This improves clarity and consistency in handling guideline data.

--- src/clippers/interfaces/guidlines.interface.ts
+++ src/clippers/interfaces/guidlines.interface.ts
@@ -1,4 +1,11 @@
-export interface GuidleinesInterface {
-    clipperId: string;
-    guideline: string;
-    }
+export interface GuidelinesInterface {
+  clipperId: string;
+  guideline: string;
+}
+export interface GuidelinesResponse {
+  id: string;
+  createdAt: string;
+  clipperId: string;
+  guideline: string;
+  updatedAt: string;
+}
