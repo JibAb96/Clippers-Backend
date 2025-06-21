286f6f5f9dcd3083e3b7bfbe4a4ead3b95bd8d00
JibAb96 <j.abshir96@gmail.com>
2025-06-21 11:01:58 +0200

    Remove unused GuidelinesService and GuidelinesRepository from ClippersModule

--- src/clippers/clippers.module.ts
+++ src/clippers/clippers.module.ts
@@ -5,8 +5,6 @@ import { PortfolioService } from './portforlio.service';
 import { ClippersRepository } from './clippers.repository';
 import { PortfolioRepository } from './portfolio.repository';
 import { SupabaseModule } from '../supabase/supabase.module';
-import { GuidelinesService } from './guidelines.service';
-import { GuidelinesRepository } from './guidlelines.repository';
 import { ClippersFacadeService } from './clippers-facade.service';
 
 @Module({
@@ -15,8 +13,6 @@ import { ClippersFacadeService } from './clippers-facade.service';
     PortfolioService,
     ClippersRepository,
     PortfolioRepository,
-    GuidelinesService,
-    GuidelinesRepository,
     ClippersFacadeService,
   ],
   controllers: [ClippersController],
