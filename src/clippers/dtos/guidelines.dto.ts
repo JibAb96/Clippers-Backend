44230ab32f216319460818ed763a088394376bf6
JibAb96 <j.abshir96@gmail.com>
2025-06-21 11:02:32 +0200

    Remove Guidelines module and related files to streamline Clippers functionality

--- src/clippers/dtos/guidelines.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsString, IsNotEmpty, MinLength, MaxLength } from 'class-validator';
-
-export class CreateGuidelineDto {
-  @IsString()
-  @IsNotEmpty()
-  @MinLength(2)
-  @MaxLength(200)
-  guideline: string;
-}
