f600efc3f0c728755d9f4d45624bc49b99b10da5
JibAb96 <j.abshir96@gmail.com>
2025-06-21 12:23:36 +0200

    Write a utility function for turning snake case key to camelcase

--- src/utility/camelToSnake.ts
+++ src/utility/camelToSnake.ts
@@ -1,10 +1,19 @@
-import { isArray, isObject, mapKeys, snakeCase } from 'lodash';
+import { camelCase, isArray, isObject, mapKeys, snakeCase } from 'lodash';
 
 export function camelToSnake(input: any): any {
   if (isArray(input)) {
     return input.map(camelToSnake);
   } else if (isObject(input)) {
     return mapKeys(input, (_value, key) => snakeCase(key));
+  }
+  return input;
+}
+
+export function snakeToCamel(input: any): any {
+  if (isArray(input)) {
+    return input.map(snakeToCamel);
+  } else if (isObject(input)) {
+    return mapKeys(input, (_value, key) => camelCase(key));
   }
   return input;
 }
