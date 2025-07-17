ce5c1a76aebe43b3abc0fe2a7f6895a55617d3d3
JibAb96 <j.abshir96@gmail.com>
2025-07-17 12:36:59 +0200

    Refactor AuthModule to include GoogleOAuthService and GoogleOnboardingService

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,17 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { AuthRepository } from "./auth.repository";
+import { AuthRepository } from './auth.repository';
 import { AuthController } from './auth.controller';
-import { CreatorsModule } from "../creators/creators.module";
-import { UserFacadeService } from "./user-facade.service";
-import { SupabaseModule } from "../supabase/supabase.module";
-import { ClippersModule } from "src/clippers/clippers.module";
+import { CreatorsModule } from '../creators/creators.module';
+import { UserFacadeService } from './user-facade.service';
+import { SupabaseModule } from '../supabase/supabase.module';
+import { ClippersModule } from 'src/clippers/clippers.module';
+import { GoogleOAuthService } from './google-oauth.service';
+import { GoogleOnboardingService } from './google-onboarding.service';
 
 @Module({
-  providers: [AuthService, UserFacadeService, AuthRepository],
-  exports: [AuthService],
-  controllers: [AuthController],
-  imports: [CreatorsModule, SupabaseModule, ClippersModule
+  providers: [
+    AuthService,
+    UserFacadeService,
+    AuthRepository,
+    GoogleOAuthService,
+    GoogleOnboardingService,
   ],
+  exports: [AuthService, UserFacadeService, GoogleOAuthService],
+  controllers: [AuthController],
+  imports: [CreatorsModule, SupabaseModule, ClippersModule],
 })
 export class AuthModule {}
